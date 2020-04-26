c4c1dc55b7cc6365380b1214b0fd097daa9febb3
Robin Panta <hacktivistic@gmail.com>
2020-04-27 02:31:15 +0530

    refactor(singlemovie): fix double click issue on Movie link
    Previously there was need to click the button two times for the link to work
    due to state of link being set when clicked the button once, then redirection on 2nd click
    The unrequired middle step of setting state was skipped

--- src/component/singlemovie.js
+++ src/component/singlemovie.js
@@ -1,26 +1,9 @@
 import React, { Component } from 'react';
 import './style.css'
 import {Link} from 'react-router-dom'
+import * as slugify from 'slugify'
 export default class Single extends Component{
-    // image=(obj)=>{
-    //     axios.get(`https://image.tmdb.org/t/p/w500`+obj).then((img)=>{
-    //         console.log(img)
-    //     })
-    // }
-  
-    state={forlink:{}}
 
-    
-    seeclicked=()=>{
-        this.setState({forlink:{
-         pathname:`movie/${this.props.movie.original_title}`,
-         aboutProps:{
-             movied: this.props.movie 
-         }
-      }
-     })
-                 
-     }
     render(){
        const src=`https://image.tmdb.org/t/p/w500`+this.props.movie.poster_path
        var str=this.props.movie.overview
@@ -28,23 +11,29 @@ export default class Single extends Component{
        if(title.length > 25) title = title.substring(0,25)+'...'
        if(str.length > 200) str = str.substring(0,200)+'...'
 
-       
+       // Directly create a Link Object for Link. Don't need to setState here. setState is useless in this case.
+       let link = {
+           pathname: `/movie/${this.props.movie.id}/${slugify(this.props.movie.original_title)}`,
+           movieProps: {
+               movie: this.props.movie
+           }
+        }
+
         return(
-            <figure class="imghvr-flip-diag-1" id="container">
-                <img id="img" src={src}/>
-                <figcaption id="inside">
-                    <h4>Title:  {this.props.movie.original_title}</h4>
-                    <h4>Release Date: {this.props.movie.release_date}</h4>
-                    <h4>Overview:</h4> 
-                    <p>{str}</p>
-                    <Link to={this.state.forlink}>
-                        <button onClick={this.seeclicked}>See More</button>
-                    </Link>
-                </figcaption>
-                <div id="detail">
-                    <h3><center> {title}</center></h3> 
-                </div>
-            </figure>
+            <Link to={link}>
+                <figure class="imghvr-flip-diag-1" id="container">
+                    <img id="img" src={src} />
+                    <figcaption id="inside">
+                        <h4>Title:  {this.props.movie.original_title}</h4>
+                        <h4>Release Date: {this.props.movie.release_date}</h4>
+                        <h4>Overview:</h4> 
+                        <p>{str}</p>
+                    </figcaption>
+                    <div id="detail">
+                        <h3><center> {title}</center></h3> 
+                    </div>
+                </figure>
+            </Link>
 
         )
     }
